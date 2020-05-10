0c20630f29378a8325ac6e84e616f103da204b46
pitekiren <pitektiren@gmail.com>
2020-05-10 09:01:19 +0700

    Author page backend finished

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 //connect to router file
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -18,11 +19,13 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(methodOverride("_method"));
 
 //Connect to mnogDB
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
